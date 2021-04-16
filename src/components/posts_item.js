7d9c0bd8a23155b53f31b1057337376412f73edd
Sheikh Tanzeela <Sheikhtanzeela590@gmail.com>
2021-04-16 22:47:56 +0530

    React-Router

---
+++ src/components/posts_item.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class PostsItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    console.log(this.props);
+
+    return <div>{this.props.match.params.id}</div>;
+  }
+}
+export default PostsItem;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,46 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-// COMPONENTS
-import Header from "./components/header";
-import JSON from "./db.json";
-import NewsList from "./components/news_list";
+import { BrowserRouter, Route, Link, Switch, NavLink } from "react-router-dom";
+
+import Posts from "./components/posts";
+import Profile from "./components/profile";
+import PostsItem from "./components/posts_item";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      news: JSON,
-      filtered: JSON,
-    };
-  }
-
-  filterNews(keywords) {
-    let filtered = this.state.news.filter((item) => {
-      return item.title.indexOf(keywords) > -1;
-    });
-    this.setState({ filtered });
-  }
-
   render() {
-    return (
-      <div>
-        <Header newsSearch={(keywords) => this.filterNews(keywords)} />
-        <NewsList news={this.state.filtered} />
-      </div>
-    );
+    return <div>Home</div>;
   }
 }
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <header>
+        <NavLink
+          to="/"
+          activeStyle={{ color: "red" }}
+          activeClassName="selected"
+        >
+          Home
+        </NavLink>
+        <br />
+        <NavLink to="/posts" activeStyle={{ color: "red" }}>
+          Posts
+        </NavLink>
+        <br />
+        <NavLink to="/profile" activeStyle={{ color: "red" }}>
+          Profile
+        </NavLink>
+        <br />
+        <hr />
+      </header>
+      <Switch>
+        <Route path="/posts/:id" component={PostsItem}></Route>
+        <Route path="/profile" component={Profile}></Route>
+        <Route path="/posts" component={Posts}></Route>
+        <Route exact path="/" component={App}></Route>
+      </Switch>
+    </div>
+  </BrowserRouter>,
+  document.querySelector("#root")
+);
